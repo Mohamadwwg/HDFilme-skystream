3df6781f058edbc720517c2bfc4390cd3c8cfc0e
Mohamadwwg <mohamad.alshekhameen@gmail.com>
2026-08-15 14:46:36 +0200

    Fix import path for skystream-extractors

--- HDFilme/plugin.js
+++ HDFilme/plugin.js
@@ -1,4 +1,4 @@
-import { Voe, StreamTape, MixDrop } from 'skystream-extractors';
+import { Voe, StreamTape, MixDrop } from 'skystream-extractors/dist/index.js'; 
 
 (function() {
     const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
